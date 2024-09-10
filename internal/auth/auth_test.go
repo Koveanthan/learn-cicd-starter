8a3591a11a74dd54807fa0198d3f5c9856f75716
Koveanthan <154501772+Koveanthan@users.noreply.github.com>
2024-09-10 09:58:58 +0530

    add changes - split auth to multiple test and coverage for ci/cd

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -13,12 +13,12 @@ func TestAuth(t *testing.T) {
 			want   string
 		}{
 			{
-				"malformed empty header",
+				"Proper key 1",
 				http.Header{"Authorization": []string{"ApiKey key"}},
 				"key",
 			},
 			{
-				"malformed empty header",
+				"Proper key 2",
 				http.Header{"Authorization": []string{"ApiKey KDLjsldkjasklj3l290"}},
 				"KDLjsldkjasklj3l290",
 			},
@@ -36,6 +36,41 @@ func TestAuth(t *testing.T) {
 			if want != got {
 				t.Errorf("Want %s, Got %s", want, got)
 			}
+		}
+	})
+
+	t.Run("get auth api should fail for cases other than ApiKey", func(t *testing.T) {
+		cases := []struct {
+			desc   string
+			header http.Header
+			want   string
+		}{
+			{
+				"auth using basic",
+				http.Header{"Authorization": []string{"Basic key"}},
+				"key",
+			},
+			{
+				"malformed auth header - no space between ApiKey and value",
+				http.Header{"Authorization": []string{"ApiKeyKDLjsldkjasklj3l290"}},
+				"KDLjsldkjasklj3l290",
+			},
+		}
+
+		for _, c := range cases {
+			header := c.header
+			_, err := GetAPIKey(header)
+			if err == nil {
+				t.Fatalf("Expected malformed header error to be thrown")
+			}
+		}
+	})
+
+	t.Run("should throw error if no header is present", func(t *testing.T) {
+		header := http.Header{}
+		_, err := GetAPIKey(header)
+		if err == nil {
+			t.Fatalf("Expected not found header error to be thrown")
 		}
 	})
 }
